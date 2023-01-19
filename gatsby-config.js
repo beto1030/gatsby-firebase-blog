05e7d7ca18e585ee045e7ba248b274eb316d6f88
Alberto Alvarado <albertoalvarado@Albertos-MacBook-Pro-2.local>
2023-01-19 01:40:26 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-firebase-blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
